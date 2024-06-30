a344afb09c2e29db13df7f3dec304b5dd93dd296
mariyarahael <mariyarahael@gmail.com>
2024-06-30 14:09:04 +0530

    done

--- app/src/main/java/com/example/studentapp/MainActivity.java
+++ app/src/main/java/com/example/studentapp/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.studentapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+AppCompatButton b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        b1=(AppCompatButton) findViewById(R.id.addstudbut);
+        b2=(AppCompatButton) findViewById(R.id.viewstudbut);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1=new Intent(getApplicationContext(), AddStudPage.class);
+                startActivity(i1);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i2=new Intent(getApplicationContext(), ViewStudPage.class);
+                startActivity(i2);
+            }
+        });
+
 
     }
 }
